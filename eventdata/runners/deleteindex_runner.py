1a549f1ea81ad81e086e875dadc1fcab16ecc88e
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2018-07-27 10:17:02 +0300

    Add challange for long-running benchmarks
    Add a new challenge `elasticlogs-continuous-index-and-query`
    suitable for long-running benchmarks. This commit also includes:
    
    * an updated version of the `deleteindex_runner.py` to help keep
      rolled-over indices to a defined size by deleting older ones.
    * updates to `README.md`.
    * a test/helper script tests/validate_challanges.py to assist with the
      JSON validation of challenges that contain embedded j2 DSL.
    
    Relates #18

--- eventdata/runners/deleteindex_runner.py
+++ eventdata/runners/deleteindex_runner.py
@@ -1,17 +1,64 @@
-import elasticsearch
+from fnmatch import fnmatch
 
 def deleteindex(es, params):
     """
-    Deletes all indices in Elasticsearch matching the specified index pattern.
+    Deletes all indices in Elasticsearch matching either the specified index pattern or
+    the suffix of the index against a more complex pattern.
 
-    It expects the parameter hash to contain the following key:
-        "index_pattern"        - Specifies the index pattern to delete. Defaults to 'elasticlogs-*'
+    :param es: Specifies the index pattern to delete. Defaults to 'elasticlogs-*'
+    :type es: str
+    :param params: Parameter hash containing one of the keys documented below.
+    :type params: dict
+
+        "index_pattern"        - Mandatory.
+                                 Specifies the index pattern to delete. Defaults to 'elasticlogs-*'
+        "max_indices"          - Optional.
+                                 int specifying how many rolled over indices to retain at max.
+                                 The elibigle indices need to satisfy `index-pattern`.
+                                 'suffix_separator' is used to retrieve the integer suffixes to calculate indices to delete.
+
+                                 Example:
+                                    For the indices: 'elasticlogs-000001', 'elasticlogs-000002', ... 000011
+                                    (index currently written to is 'elasticlogs-000011')
+
+                                    using:
+                                        suffix_separator='-' and
+                                        max_indices=8
+
+                                    will result in deleting indices 'elasticlogs-000001' and 'elasticlogs-000002'
+        "suffix_separator"     - Defaults to '-'. Used only when 'max_indices' is specified.
+                                 Specifies string separator used to extract the index suffix, e.g. '-'.
+
+
     """
-    if 'index_pattern' in params:
-        index_pattern = params['index_pattern']
-    else:
-        index_pattern = "elasticlogs-*"
+    def get_suffix(name, separator):
+        if separator in name:
+            name_parts = name.split(separator)
+            if len(name_parts) > 1:
+                try:
+                    return int(name_parts[-1])
+                except ValueError:
+                    # TODO: log that suffix is not integer
+                    return None
+        return None
 
-    es.indices.delete(index=index_pattern)
+    index_pattern = params.get('index_pattern', 'elasticlogs-*')
+    max_indices = params.get('max_indices', None)
+    suffix_separator = params.get('suffix_separator', '-')
+
+    if max_indices:
+        indices = es.cat.indices(h='index').split("\n")
+        indices_by_suffix = {get_suffix(idx, suffix_separator): idx
+            for idx in indices
+            if fnmatch(idx, index_pattern) and
+            get_suffix(idx, suffix_separator) is not None
+        }
+
+        sorted_suffixes = sorted(list(indices_by_suffix.keys()))
+        if len(sorted_suffixes) > max_indices:
+            indices_to_delete = ",".join([indices_by_suffix[key] for key in sorted_suffixes[:(len(sorted_suffixes)-max_indices)]])
+            es.indices.delete(indices_to_delete)
+    else:
+        es.indices.delete(index=index_pattern)
 
     return 1, "ops"
--- eventdata/track.py
+++ eventdata/track.py
@@ -1,23 +1,25 @@
 from eventdata.parameter_sources.elasticlogs_bulk_source import ElasticlogsBulkSource
 from eventdata.parameter_sources.elasticlogs_kibana_source import ElasticlogsKibanaSource
-from eventdata.parameter_sources.sample_based_bulk_source import SampleBasedBulkSource
 from eventdata.parameter_sources.interval_query_source import IntervalQuerySource
-from eventdata.runners import rollover_runner
+from eventdata.parameter_sources.sample_based_bulk_source import SampleBasedBulkSource
 from eventdata.runners import createindex_runner
-from eventdata.runners import kibana_runner
-from eventdata.runners import indicesstats_runner
-from eventdata.runners import nodestorage_runner
+from eventdata.runners import deleteindex_runner
 from eventdata.runners import fieldstats_runner
+from eventdata.runners import indicesstats_runner
+from eventdata.runners import kibana_runner
+from eventdata.runners import nodestorage_runner
+from eventdata.runners import rollover_runner
 
 
 def register(registry):
     registry.register_param_source("elasticlogs_bulk", ElasticlogsBulkSource)
     registry.register_param_source("elasticlogs_kibana", ElasticlogsKibanaSource)
-    registry.register_param_source("sample_based_bulk", SampleBasedBulkSource)
     registry.register_param_source("interval_query", IntervalQuerySource)
-    registry.register_runner("rollover", rollover_runner.rollover)
+    registry.register_param_source("sample_based_bulk", SampleBasedBulkSource)
     registry.register_runner("createindex", createindex_runner.createindex)
-    registry.register_runner("kibana", kibana_runner.kibana)
-    registry.register_runner("indicesstats", indicesstats_runner.indicesstats)
-    registry.register_runner("node_storage", nodestorage_runner.nodestorage)
+    registry.register_runner("delete_indices", deleteindex_runner.deleteindex)
     registry.register_runner("fieldstats", fieldstats_runner.fieldstats)
+    registry.register_runner("indicesstats", indicesstats_runner.indicesstats)
+    registry.register_runner("kibana", kibana_runner.kibana)
+    registry.register_runner("node_storage", nodestorage_runner.nodestorage)
+    registry.register_runner("rollover", rollover_runner.rollover)
