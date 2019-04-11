6c2c9fc5d71111346b41fca54d6ee010c684c251
Dimitrios Liappis <dimitrios.liappis@gmail.com>
2019-04-11 17:28:22 +0300

    Add support for updates in customer parameter source
    and commit bulk-update challenge
    
    Relates #34

--- eventdata/parameter_sources/elasticlogs_bulk_source.py
+++ eventdata/parameter_sources/elasticlogs_bulk_source.py
@@ -1,6 +1,6 @@
 import logging
-import random
 import uuid
+import random
 import time
 import hashlib
 import base64
@@ -14,55 +14,60 @@ class ElasticlogsBulkSource:
     Generates a bulk indexing request for elasticlogs data.
 
     It expects the parameter hash to contain the following keys:
-        "bulk-size"            -    Integer indicating events generated per bulk request. Defaults to 1000.
-        "index"                -    Name of index, index prefix or alias documents should be indexed into. The index name
-                                    can be made to generate time based indices by including date formatting in the name.
-                                    'test-<yyyy>-<mm>-<dd>-<hh>' will generate an hourly index. (mandatory)
-        "starting_point"       -    String specifying the starting point for event time generation. It supports absolute or
-                                    relative values as follows:
-                                        'now'                 - Always evaluated to the current timestamp at time of generation
-                                        'now-1h'              - Offset to the current timestamp. Consists of a number and 
-                                                                either m (minutes), h (hours) or d (days).
-                                        '2017-02-20 20:12:32' - Exact timestamp.
-                                        '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
-                                    If a relative starting point (based on now) is provided, this will be used for generation.
-                                    In the case an exact timestamp is provided as starting point, the difference to now will
-                                    be calculated when the generation starts and this will be used as an offset for all events. 
-                                    If an interval is provided by also specifying an end_point, the range will be calculated for 
-                                    each bulk request and each event will be assigned a random timestamp withion this range.
-                                    starting point. Defaults to 'now'.
-        "end_point"            -    String specifying the end point for event time generation. It supports absolute or
-                                    relative values as follows:
-                                        'now'                 - Always evaluated to the current timestamp at time of generation
-                                        'now-1h'              - Offset to the current timestamp. Consists of a number and 
-                                                                either m (minutes), h (hours) or d (days).
-                                        '2017-02-20 20:12:32' - Exact timestamp.
-                                        '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
-                                    When specified, the event timestamp will be generated randomly with in the interval defined 
-                                    by the starting_point and end_point parameters. If end_poiunt < starting_point, they will be 
-                                    swapped. 
-        "acceleration_factor"  -    This factor only applies when an exact timestamp or date has been provided as starting point 
-                                    and no end_point has been defined. It allows the time progression in the timestamp calculation 
-                                    to be altered. A value larger than 1 will accelerate generation and a value lower than 1 will 
-                                    slow it down. If a task is set up to run indexing for one hour with a fixed starting point of 
-                                    '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be generated in timestamp 
-                                    sequence covering a 2-hour window, '2017-02-20 20:12:32' to '2017-02-20 22:12:32' (approximately).
-        "id_type"              -    Type of document id to use for generated documents. Defaults to `auto`.
-                                        auto         - Do not explicitly set id and let Elasticsearch assign automatically.
-                                        uuid         - Assign a UUID4 id to each document.
-                                        epoch_uuid   - Assign a UUIO4 identifier prefixed with the hex representation of the current 
-                                                       timestamp.
-                                        epoch_md5    - Assign a base64 encoded MD5 hash of a UUID prefixed with the hex representation 
-                                                       of the current timestamp. (Note: Generating this type of id can be CPU intensive)
-                                        md5          - MD5 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
-                                        sha1         - SHA1 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
-                                        sha256       - SHA256 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
-                                        sha384       - SHA384 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
-                                        sha512       - SHA512 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
-        "id_delay_probability" -    If id_type is set to `epoch_uuid` this parameter determnines the probability will be set in the 
-                                    past. This can be used to simulate a portion of the events arriving delayed. Must be in range [0.0, 1.0].
-                                    Defaults to 0.0.
-        "id_delay_secs".       -    If an event is delayed, this number of seconds will be deducted from the current timestamp.
+        "bulk-size"                -    Integer indicating events generated per bulk request. Defaults to 1000.
+        "index"                    -    Name of index, index prefix or alias documents should be indexed into. The index name
+                                        can be made to generate time based indices by including date formatting in the name.
+                                        'test-<yyyy>-<mm>-<dd>-<hh>' will generate an hourly index. (mandatory)
+        "starting_point"           -    String specifying the starting point for event time generation. It supports absolute or
+                                        relative values as follows:
+                                            'now'                 - Always evaluated to the current timestamp at time of generation
+                                            'now-1h'              - Offset to the current timestamp. Consists of a number and
+                                                                    either m (minutes), h (hours) or d (days).
+                                            '2017-02-20 20:12:32' - Exact timestamp.
+                                            '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
+                                        If a relative starting point (based on now) is provided, this will be used for generation.
+                                        In the case an exact timestamp is provided as starting point, the difference to now will
+                                        be calculated when the generation starts and this will be used as an offset for all events.
+                                        If an interval is provided by also specifying an end_point, the range will be calculated for
+                                        each bulk request and each event will be assigned a random timestamp withion this range.
+                                        starting point. Defaults to 'now'.
+        "end_point"                -    String specifying the end point for event time generation. It supports absolute or
+                                        relative values as follows:
+                                            'now'                 - Always evaluated to the current timestamp at time of generation
+                                            'now-1h'              - Offset to the current timestamp. Consists of a number and
+                                                                    either m (minutes), h (hours) or d (days).
+                                            '2017-02-20 20:12:32' - Exact timestamp.
+                                            '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
+                                        When specified, the event timestamp will be generated randomly with in the interval defined
+                                        by the starting_point and end_point parameters. If end_poiunt < starting_point, they will be
+                                        swapped.
+        "acceleration_factor"      -    This factor only applies when an exact timestamp or date has been provided as starting point
+                                        and no end_point has been defined. It allows the time progression in the timestamp calculation
+                                        to be altered. A value larger than 1 will accelerate generation and a value lower than 1 will
+                                        slow it down. If a task is set up to run indexing for one hour with a fixed starting point of
+                                        '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be generated in timestamp
+                                        sequence covering a 2-hour window, '2017-02-20 20:12:32' to '2017-02-20 22:12:32' (approximately).
+        "id_type"                  -    Type of document id to use for generated documents. Defaults to `auto`.
+                                            auto         - Do not explicitly set id and let Elasticsearch assign automatically.
+                                            seq          - Assign sequentialy incrementing integer ids to each document.
+                                            uuid         - Assign a UUID4 id to each document.
+                                            epoch_uuid   - Assign a UUIO4 identifier prefixed with the hex representation of the current
+                                                           timestamp.
+                                            epoch_md5    - Assign a base64 encoded MD5 hash of a UUID prefixed with the hex representation
+                                                           of the current timestamp. (Note: Generating this type of id can be CPU intensive)
+                                            md5          - MD5 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
+                                            sha1         - SHA1 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
+                                            sha256       - SHA256 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
+                                            sha384       - SHA384 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
+                                            sha512       - SHA512 hash of UUID in hex representation. (Note: Generating this type of id can be CPU intensive)
+        "id_seq_probability"       -    If set, the probability an existing id will be used to simulate an update.
+                                            Applied only when `id_type` is seq.
+                                            Defaults to 0.0 which brings no updates. Must be in range [0.0, 1.0].
+        "id_seq_low_id_bias"       -    If set, favor low ids with a very high bias. Must be True/False. Default is False.
+        "id_delay_probability"     -    If id_type is set to `epoch_uuid` this parameter determnines the probability will be set in the
+                                        past. This can be used to simulate a portion of the events arriving delayed. Must be in range [0.0, 1.0].
+                                        Defaults to 0.0.
+        "id_delay_secs"            -    If an event is delayed, this number of seconds will be deducted from the current timestamp.
     """
     def __init__(self, track, params, **kwargs):
         self._indices = track.indices
@@ -75,8 +80,10 @@ class ElasticlogsBulkSource:
             self._bulk_size = params['bulk-size']
 
         self._id_type = "auto"
+        self.seq_id = 0
+
         if 'id_type' in params.keys():
-            if params['id_type'] in ['auto', 'uuid', 'epoch_uuid', 'epoch_md5', 'md5', 'sha1', 'sha256', 'sha384', 'sha512']:
+            if params['id_type'] in ['auto', "seq", 'uuid', 'epoch_uuid', 'epoch_md5', 'md5', 'sha1', 'sha256', 'sha384', 'sha512']:
                 self._id_type = params['id_type']
             else:
                 logger.warning("[bulk] Invalid id_type ({}) specified. Will use default.".format(params['id_type']))
@@ -92,6 +99,15 @@ class ElasticlogsBulkSource:
             else:
                 self._id_delay_secs = 0
 
+        if self._id_type == "seq":
+            self.orig_args = [track, params, kwargs]
+            self._id_seq_probability = float(params['id_seq_probability']) if 'id_seq_probability' in params else 0.0
+            self._low_id_bias = str(params.get('id_seq_low_id_bias', False)).lower() == "true"
+            if self._low_id_bias:
+                logger.info("Will use low id bias for updates")
+            else:
+                logger.info("Will use uniform distribution for updates")
+
         self._default_index = False
         if 'index' not in params.keys():
             if len(self._indices) > 1:
@@ -106,9 +122,14 @@ class ElasticlogsBulkSource:
             logger.debug("[bulk] Index pattern specified in parameters ({}) will be used".format(params['index']))
 
     def partition(self, partition_index, total_partitions):
-        seed = partition_index * self._params["seed"] if "seed" in self._params else None
-        random.seed(seed)   
-        return self
+        if self._params.get("id_type") == "seq":
+            new_params = copy.deepcopy(self.orig_args[1])
+            new_params["client_id"] = partition_index
+            return ElasticlogsBulkSource(self.orig_args[0], new_params, **self.orig_args[2])
+        else:
+            seed = partition_index * self._params["seed"] if "seed" in self._params else None
+            random.seed(seed)
+            return self
 
     def size(self):
         return 1
@@ -124,6 +145,8 @@ class ElasticlogsBulkSource:
             else:
                 if self._id_type == 'uuid':
                     docid = self.__get_uuid()
+                elif self._id_type == "seq":
+                    docid = "%s-%d" % (self.__get_seq_id(), self._params["client_id"])
                 elif self._id_type == 'sha1':
                     docid = hashlib.sha1(self.__get_uuid().encode('utf8')).hexdigest()
                 elif self._id_type == 'sha256':
@@ -138,7 +161,7 @@ class ElasticlogsBulkSource:
                     docid = self.__get_epoch_md5()
                 else:
                     docid = self.__get_epoch_uuid()
-                
+
                 bulk_array.append('{"index": {"_index": "%s", "_type": "doc", "_id": "%s"}}"' % (idx, docid))
 
             bulk_array.append(evt)
@@ -157,7 +180,7 @@ class ElasticlogsBulkSource:
         u = self.__get_uuid()
         ts = int(time.time())
 
-        if(self._id_delay_probability > 0 and self._id_delay_probability < random.random()):
+        if 0 < self._id_delay_probability < random.random():
             ts = ts - self._id_delay_secs
 
         return '{:x}{}'.format(ts, u)
@@ -167,7 +190,30 @@ class ElasticlogsBulkSource:
         md5_str = str(base64.urlsafe_b64encode(hashlib.md5(u.encode('utf8')).digest()))[2:24]
         ts = int(time.time())
 
-        if(self._id_delay_probability > 0 and self._id_delay_probability < random.random()):
+        if 0 < self._id_delay_probability < random.random():
             ts = ts - self._id_delay_secs
 
         return hex(ts)[2:10] + md5_str
+
+    def __get_seq_id(self):
+        _id = self.seq_id
+        if random.uniform(0, 1) < self._id_seq_probability:
+            # conflict
+            if self._low_id_bias:
+                # update; heavily bias towards older ids
+                _p = 10
+                _min = 0
+                _max = _id
+                # _p ~> 0: results closer to min, _p >> 0: results closer to max
+                _id = _min + (_max - _min) * pow(random.random(), _p)
+            else:
+                # update; pick id from pure uniform distribution
+                _id = random.randint(0, _id-1 if _id > 0 else 0)
+        else:
+            # new document
+            self.__incr_seq_id()
+
+        return "%012d" % _id
+
+    def __incr_seq_id(self):
+        self.seq_id += 1
