f1824a74f469d9977153e64e1063bd9a03089eb1
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2019-04-29 16:38:09 +0200

    Simplify fieldstats runner

--- eventdata/runners/fieldstats_runner.py
+++ eventdata/runners/fieldstats_runner.py
@@ -1,43 +1,49 @@
 from eventdata.utils import globals as gs
 import logging
 
-logger = logging.getLogger("track.eventdata")
-
-class Error(Exception):
-    """Base class for exceptions in this module."""
-    pass
-
-class ParameterError(Error):
-    """Exception raised for parameter errors.
-    Attributes:
-        message -- explanation of the error
-    """
-
-    def __init__(self, message):
-        self.message = message
 
 def fieldstats(es, params):
     """
     Looks up minimum and maximum values for a specified field for an index pattern and stores
-    this inform ation in a globval variable that can be accessed by other componenets of the track.
-    It expects the parameter hash to contain the following keys:
-        "index_pattern" - Index pattern statistics are retrieved for. Defaults to "filebeat-*".
-        "fieldname" - Field to extract statistics for. Defaults to "@timestamp".
-    """
-    if 'index_pattern' not in params:
-        params['index_pattern'] = 'elasticlogs-*'
-    
-    if 'fieldname' not in params:
-        params['fieldname'] = '@timestamp'
+    this information in a global variable that can be accessed by other components of the track.
 
-    ignore_throttled = params.get('ignore_throttled', True)
+    It expects a parameter dictionary with the following keys:
+
+    * index_pattern (mandatory): Index pattern statistics are retrieved for.
+    * fieldname (optional): Field to extract statistics for. Defaults to "@timestamp".
+
+    """
+    index_pattern = params["index_pattern"]
+    field_name = params.get("fieldname", "@timestamp")
+    ignore_throttled = params.get("ignore_throttled", True)
 
     if ignore_throttled:
-        result = es.search(index=params['index_pattern'], body={"query": {"match_all": {}},"aggs" : {"maxval" : { "max" : { "field" : params['fieldname'] } },"minval" : { "min" : { "field" : params['fieldname'] } }},"size":0})
+        query_params = {}
     else:
-        result = es.search(index=params['index_pattern'], body={"query": {"match_all": {}},"aggs" : {"maxval" : { "max" : { "field" : params['fieldname'] } },"minval" : { "min" : { "field" : params['fieldname'] } }},"size":0}, params={'ignore_throttled': 'false'})
+        query_params = {"ignore_throttled": "false"}
 
-    hits = result['hits']['total']
+    result = es.search(index=index_pattern,
+                       body={
+                           "query": {
+                               "match_all": {}
+                           },
+                           "size": 0,
+                           "aggs": {
+                               "maxval": {
+                                   "max": {
+                                       "field": field_name
+                                   }
+                               },
+                               "minval": {
+                                   "min": {
+                                       "field": field_name
+                                   }
+                               }
+                           }
+                       },
+                       params=query_params)
+
+    hits = result["hits"]["total"]
     # ES 7.0+
     if isinstance(hits, dict):
         total_hits = hits["value"]
@@ -45,9 +51,15 @@ def fieldstats(es, params):
         total_hits = hits
 
     if total_hits > 0:
-        key = "{}_{}".format(params['index_pattern'], params['fieldname']);
-        gs.global_fieldstats[key] = {'max': int(result['aggregations']['maxval']['value']), 'min': int(result['aggregations']['minval']['value'])};
-
-        logger.info("[fieldstats] Identified statistics for field '{}' in '{}'. Min: {}, Max: {}".format(params['fieldname'], params['index_pattern'], int(result['aggregations']['minval']['value']), int(result['aggregations']['maxval']['value'])))
+        key = "{}_{}".format(index_pattern, field_name)
+        min_field_value = int(result["aggregations"]["minval"]["value"])
+        max_field_value = int(result["aggregations"]["maxval"]["value"])
+        gs.global_fieldstats[key] = {
+            "max": max_field_value,
+            "min": min_field_value
+        }
+        logger = logging.getLogger("track.eventdata.fieldstats")
+        logger.info("Identified statistics for field '%s' in '%s'. Min: %d, Max: %d",
+                    field_name, index_pattern, min_field_value, max_field_value)
     else:
-        raise ParameterError("No matching data found for field '{}' in pattern '{}'.".format(params['fieldname'], params['index_pattern']));
+        raise AssertionError("No matching data found for field '{}' in pattern '{}'.".format(field_name, index_pattern))
