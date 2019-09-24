elastic/rally-eventdata-track

commit 863d532898db476ba67e336039b4d53b5899e4d8

Add challenge to index fixed amount of daily logs
With this commit we add a new challenge `index-logs-fixed-daily-volume` which
allows to ingest a fixed (but parameterizable) amount of raw logs per day. Also,
the number of days can be specified.

We also add a new parameter `daily_logging_volume` that allows users to limit
the amount of (raw) data that is generated per (logical) day. If the limit is
hit, we simulate that the next document is generated on the next day. With
another new parameter `number_of_days` we can configure for how many days logs
should be generated.

We also remove support to specify an `end_point` in the timestamp generator. 
This functionality was only used in one place and has also generated an
unrealistic distribution of timestamps (random across the whole range) and
should instead be simulated with an acceleration factor.

Finally, we materialize relative timestamps eagerly so we can easily
generate timestamps on the hot code path without making any further decisions.
This slightly changes the semantics of the start timestamp of the timestamp
generator (it's now evaluated when the object is created instead of when its
generator method is first invoked) but this does not matter in practice as both
calls happen within a very short time period.

Relates #38

eventdata/parameter_sources/elasticlogs_bulk_source.py
@@ -47,25 +47,13 @@ class ElasticlogsBulkSource:
                                         If a relative starting point (based on now) is provided, this will be used for generation.
                                         In the case an exact timestamp is provided as starting point, the difference to now will
                                         be calculated when the generation starts and this will be used as an offset for all events.
-                                        If an interval is provided by also specifying an end_point, the range will be calculated for
-                                        each bulk request and each event will be assigned a random timestamp withion this range.
-                                        starting point. Defaults to 'now'.
-        "end_point"                -    String specifying the end point for event time generation. It supports absolute or
-                                        relative values as follows:
-                                            'now'                 - Always evaluated to the current timestamp at time of generation
-                                            'now-1h'              - Offset to the current timestamp. Consists of a number and
-                                                                    either m (minutes), h (hours) or d (days).
-                                            '2017-02-20 20:12:32' - Exact timestamp.
-                                            '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
-                                        When specified, the event timestamp will be generated randomly with in the interval defined
-                                        by the starting_point and end_point parameters. If end_poiunt < starting_point, they will be
-                                        swapped.
-        "acceleration_factor"      -    This factor only applies when an exact timestamp or date has been provided as starting point
-                                        and no end_point has been defined. It allows the time progression in the timestamp calculation
-                                        to be altered. A value larger than 1 will accelerate generation and a value lower than 1 will
-                                        slow it down. If a task is set up to run indexing for one hour with a fixed starting point of
-                                        '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be generated in timestamp
-                                        sequence covering a 2-hour window, '2017-02-20 20:12:32' to '2017-02-20 22:12:32' (approximately).
+                                        Defaults to 'now'.
+        "acceleration_factor"  -    This factor allows the time progression in the timestamp calculation to be altered.
+                                    A value larger than 1 will accelerate generation and a value lower than 1 will slow
+                                    it down. If a task is set up to run indexing for one hour with a fixed starting
+                                    point of '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be
+                                    generated in timestamp sequence covering a 2-hour window, '2017-02-20 20:12:32'
+                                    to '2017-02-20 22:12:32' (approximately).
         "id_type"                  -    Type of document id to use for generated documents. Defaults to `auto`.
                                             auto         - Do not explicitly set id and let Elasticsearch assign automatically.
                                             seq          - Assign sequentialy incrementing integer ids to each document.
@@ -89,8 +77,8 @@ class ElasticlogsBulkSource:
         "id_delay_secs"            -    If an event is delayed, this number of seconds will be deducted from the current timestamp.
     """
     def __init__(self, track, params, **kwargs):
+        self.orig_args = [track, params, kwargs]
         self._indices = track.indices
-        self._params = params
         self._params = params
         self._randomevent = RandomEvent(params)
 
@@ -119,7 +107,6 @@ class ElasticlogsBulkSource:
                 self._id_delay_secs = 0
 
         if self._id_type == "seq":
-            self.orig_args = [track, params, kwargs]
             self._id_seq_probability = float(params['id_seq_probability']) if 'id_seq_probability' in params else 0.0
             self._low_id_bias = str(params.get('id_seq_low_id_bias', False)).lower() == "true"
             if self._low_id_bias:
@@ -141,14 +128,13 @@ class ElasticlogsBulkSource:
             logger.debug("[bulk] Index pattern specified in parameters ({}) will be used".format(params['index']))
 
     def partition(self, partition_index, total_partitions):
-        if self._params.get("id_type") == "seq":
-            new_params = copy.deepcopy(self.orig_args[1])
-            new_params["client_id"] = partition_index
-            return ElasticlogsBulkSource(self.orig_args[0], new_params, **self.orig_args[2])
-        else:
+        if self._params.get("id_type") != "seq":
             seed = partition_index * self._params["seed"] if "seed" in self._params else None
             random.seed(seed)
-            return self
+        new_params = copy.deepcopy(self.orig_args[1])
+        new_params["client_id"] = partition_index
+        new_params["client_count"] = total_partitions
+        return ElasticlogsBulkSource(self.orig_args[0], new_params, **self.orig_args[2])
 
     def size(self):
         return 1
@@ -157,7 +143,15 @@ class ElasticlogsBulkSource:
         # Build bulk array
         bulk_array = []
         for x in range(0, self._bulk_size):
-            evt, idx, typ = self._randomevent.generate_event()
+            try:
+                evt, idx, typ = self._randomevent.generate_event()
+            except StopIteration:
+                if len(bulk_array) > 0:
+                    # return any remaining items if there are any (otherwise we'd lose the last bulk request)
+                    break
+                else:
+                    # otherwise stop immediately
+                    raise
 
             if self._id_type == 'auto':
                 bulk_array.append('{"index": {"_index": "%s"}}"' % (idx))
@@ -185,7 +179,11 @@ class ElasticlogsBulkSource:
 
             bulk_array.append(evt)
 
-        response = { "body": "\n".join(bulk_array), "action-metadata-present": True, "bulk-size": self._bulk_size }
+        response = {
+            "body": "\n".join(bulk_array),
+            "action-metadata-present": True,
+            "bulk-size": len(bulk_array)
+        }
 
         if "pipeline" in self._params.keys():
             response["pipeline"] = self._params["pipeline"]

eventdata/parameter_sources/metricbeat_bulk_source.py
@@ -45,25 +45,13 @@ class MetricbeatBulkSource:
                                     If a relative starting point (based on now) is provided, this will be used for generation.
                                     In the case an exact timestamp is provided as starting point, the difference to now will
                                     be calculated when the generation starts and this will be used as an offset for all events. 
-                                    If an interval is provided by also specifying an end_point, the range will be calculated for 
-                                    each bulk request and each event will be assigned a random timestamp withion this range.
-                                    starting point. Defaults to 'now'.
-        "end_point"            -    String specifying the end point for event time generation. It supports absolute or
-                                    relative values as follows:
-                                        'now'                 - Always evaluated to the current timestamp at time of generation
-                                        'now-1h'              - Offset to the current timestamp. Consists of a number and 
-                                                                either m (minutes), h (hours) or d (days).
-                                        '2017-02-20 20:12:32' - Exact timestamp.
-                                        '2017-02-20'          - Date. Time will be assumed to be 00:00:00.
-                                    When specified, the event timestamp will be generated randomly with in the interval defined 
-                                    by the starting_point and end_point parameters. If end_poiunt < starting_point, they will be 
-                                    swapped. 
-        "acceleration_factor"  -    This factor only applies when an exact timestamp or date has been provided as starting point 
-                                    and no end_point has been defined. It allows the time progression in the timestamp calculation 
-                                    to be altered. A value larger than 1 will accelerate generation and a value lower than 1 will 
-                                    slow it down. If a task is set up to run indexing for one hour with a fixed starting point of 
-                                    '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be generated in timestamp 
-                                    sequence covering a 2-hour window, '2017-02-20 20:12:32' to '2017-02-20 22:12:32' (approximately).
+                                    Defaults to 'now'.
+        "acceleration_factor"  -    This factor allows the time progression in the timestamp calculation to be altered.
+                                    A value larger than 1 will accelerate generation and a value lower than 1 will slow
+                                    it down. If a task is set up to run indexing for one hour with a fixed starting
+                                    point of '2016-12-20 20:12:32' and an acceleration factor of 2.0, events will be
+                                    generated in timestamp sequence covering a 2-hour window, '2017-02-20 20:12:32'
+                                    to '2017-02-20 22:12:32' (approximately).
         "id_type"              -    Type of document id to use for generated documents. Defaults to `auto`.
                                         auto         - Do not explicitly set id and let Elasticsearch assign automatically.
                                         uuid         - Assign a UUID4 id to each document.

eventdata/parameter_sources/randomevent.py
@@ -16,16 +16,19 @@
 # under the License.
 
 
-import json
-import random
+import datetime
 import gzip
-import re
+import json
 import os
-from eventdata.utils import elasticlogs_bulk_source as ebs
-from eventdata.parameter_sources.weightedarray import WeightedArray
+import random
+import re
+
 from eventdata.parameter_sources.timeutils import TimestampStructGenerator
+from eventdata.parameter_sources.weightedarray import WeightedArray
+from eventdata.utils import elasticlogs_bulk_source as ebs
 
 cwd = os.path.dirname(__file__)
+
 
 class Agent:
     def __init__(self):
@@ -84,7 +87,6 @@ class Agent:
             with gzip.open('%s/data/agent_lookup.json.gz' % cwd, 'rt') as data_file:
                 self._agent_lookup = json.load(data_file)
             ebs.global_lookups['_agent_lookup'] = self._agent_lookup
-
 
     def add_fields(self, event):
         agent = self._agents.get_random()
@@ -154,7 +156,6 @@ class ClientIp:
             with gzip.open('%s/data/clientips_city_name_lookup.json.gz' % cwd, 'rt') as data_file:
                 self._clientips_city_name_lookup = json.load(data_file)
             ebs.global_lookups['_clientips_city_name_lookup'] = self._clientips_city_name_lookup
-
 
     def add_fields(self, event):
         p = random.random()
@@ -234,12 +235,32 @@ class Request:
         event['httpversion'] = data[5]
 
 
+def convert_to_bytes(size):
+    matched_size = re.match(r"^(\d+)\s?(kB|MB|GB)?$", size)
+    if matched_size:
+        value = int(matched_size.group(1))
+        unit = matched_size.group(2)
+        if unit == "kB":
+            return value << 10
+        elif unit == "MB":
+            return value << 20
+        elif unit == "GB":
+            return value << 30
+        elif unit is None:
+            return value
+        else:
+            # we should only reach this if the regex does not match the code here
+            raise ValueError("Unrecognized unit [{}] for byte size value [{}]".format(unit, size))
+    else:
+        raise ValueError("Invalid byte size value [{}]".format(size))
+
+
 class RandomEvent:
-    def __init__(self, params):
-        self._agent = Agent()
-        self._clientip = ClientIp()
-        self._referrer = Referrer()
-        self._request = Request()
+    def __init__(self, params, agent=Agent, client_ip=ClientIp, referrer=Referrer, request=Request):
+        self._agent = agent()
+        self._clientip = client_ip()
+        self._referrer = referrer()
+        self._request = request()
         # We will reuse the event dictionary. This assumes that each field will be present (and thus overwritten) in each event.
         # This reduces object churn and improves peak indexing throughput.
         self._event = {}
@@ -260,21 +281,32 @@ class RandomEvent:
         self._type = "doc"
         self._timestamp_generator = TimestampStructGenerator(
             params.get("starting_point", "now"),
-            params.get("end_point"),
-            float(params.get("acceleration_factor", "1.0"))
+            float(params.get("acceleration_factor", "1.0")),
+            # this is only expected to be used in tests
+            params.get("__utc_now")
         )
-
+        if "daily_logging_volume" in params and "client_count" in params:
+            # in bytes
+            self.daily_logging_volume = convert_to_bytes(params["daily_logging_volume"]) // int(params["client_count"])
+            self.current_logging_volume = 0
+            self.remaining_days = params.get("number_of_days")
+        else:
+            self.daily_logging_volume = None
+            self.current_logging_volume = 0
+            self.remaining_days = None
         self.record_raw_event_size = params.get("record_raw_event_size", False)
 
     def generate_event(self):
-        timestruct = self._timestamp_generator.generate_timestamp_struct()
+        if self.remaining_days == 0:
+            raise StopIteration()
+        timestruct = self._timestamp_generator.next_timestamp()
         index_name = self.__generate_index_pattern(timestruct)
 
         event = self._event
         event["@timestamp"] = timestruct["iso"]
 
         # set random offset
-        event["offset"] = random.randrange(0,10000000)
+        event["offset"] = random.randrange(0, 10000000)
 
         self._agent.add_fields(event)
         self._clientip.add_fields(event)
@@ -284,14 +316,23 @@ class RandomEvent:
         # set host name
         event["hostname"] = "web-{}-{}.elastic.co".format(event["geoip_continent_code"], random.randrange(1, 3))
 
-        # determine the raw event size (as if this were contained in nginx log file. We do not bother to
-        # reformat the timestamp as this is not worth the overhead.
-        if self.record_raw_event_size:
+        if self.record_raw_event_size or self.daily_logging_volume:
+            # determine the raw event size (as if this were contained in nginx log file). We do not bother to
+            # reformat the timestamp as this is not worth the overhead.
             raw_event = '%s - - [%s] "%s %s HTTP/%s" %s %s "%s" "%s"' % (event["clientip"], event["@timestamp"],
                                                                          event["verb"], event["request"],
                                                                          event["httpversion"], event["response"],
                                                                          event["bytes"], event["referrer"],
                                                                          event["agent"])
+            if self.daily_logging_volume:
+                self.current_logging_volume += len(raw_event)
+                if self.current_logging_volume > self.daily_logging_volume:
+                    if self.remaining_days is not None:
+                        self.remaining_days -= 1
+                    self._timestamp_generator.skip(datetime.timedelta(days=1))
+                    self.current_logging_volume = 0
+
+        if self.record_raw_event_size:
             # we are on the hot code path here and thus we want to avoid conditionally creating strings so we duplicate
             # the event.
             line = '{"@timestamp": "%s", ' \

eventdata/parameter_sources/timeutils.py
@@ -18,7 +18,6 @@
 
 import datetime
 import re
-import random
 
 epoch = datetime.datetime.utcfromtimestamp(0)
 
@@ -34,45 +33,27 @@ class TimeParsingError(Exception):
 
 
 class TimestampStructGenerator:
-    def __init__(self, starting_point, end_point=None, acceleration_factor=1.0, utcnow=datetime.datetime.utcnow):
-        self._start_dt = None
+    def __init__(self, starting_point, acceleration_factor=1.0, utcnow=None):
+        self._utcnow = utcnow if utcnow else datetime.datetime.utcnow
+        # the (actual) time when this generator has started
+        self._start = self._utcnow()
+        # the logical point in time for which we'll generate timestamps
         self._starting_point = self.__parse_point_def(starting_point)
-        if end_point is None:
-            self._end_point = None
-        else:
-            self._end_point = self.__parse_point_def(end_point)
         self._acceleration_factor = acceleration_factor
-        self._utcnow = utcnow
         # reuse to reduce object churn
         self._ts = {}
 
-    def generate_timestamp_struct(self):
-        if self._end_point is None:
-            if self._starting_point["type"] == "relative":
-                dt = self._utcnow() + self._starting_point["offset"]
-            else:
-                self.__set_start_dt_if_not_set()
-                td = (self._utcnow() - self._start_dt) * self._acceleration_factor
-                dt = self._starting_point["dt"] + td
-        else:
-            if self._starting_point["type"] == "relative":
-                dt1 = self._utcnow() + self._starting_point["offset"]
-            else:
-                dt1 = self._starting_point["dt"]
+    def next_timestamp(self):
+        delta = (self._utcnow() - self._start) * self._acceleration_factor
+        return self.__to_struct(self._starting_point + delta)
 
-            if self._end_point["type"] == "relative":
-                dt2 = self._utcnow() + self._end_point["offset"]
-            else:
-                dt2 = self._end_point["dt"]
+    def skip(self, delta):
+        # advance the generated timestamp by delta
+        self._starting_point = self._starting_point + delta
+        # also reset the generator start as we want to ensure the same delta in #next_timestamp()
+        self._start = self._utcnow()
 
-            interval_length = (dt2 - dt1)
-            random_offset = interval_length * random.random()
-
-            dt = dt1 + random_offset
-
-        return self.__generate_timestamp_struct_from_datetime(dt)
-
-    def __generate_timestamp_struct_from_datetime(self, dt):
+    def __to_struct(self, dt):
         # string formatting is about 4 times faster than strftime.
         iso = "%04d-%02d-%02dT%02d:%02d:%02d.%03dZ" % (dt.year, dt.month, dt.day, dt.hour, dt.minute, dt.second, dt.microsecond)
         self._ts["iso"] = iso
@@ -83,46 +64,42 @@ class TimestampStructGenerator:
         self._ts["hh"] = iso[11:13]
         return self._ts
 
-    def __set_start_dt_if_not_set(self):
-        if self._start_dt is None:
-            self._start_dt = self._utcnow()
-
     def __parse_point_def(self, point):
         if point == "now":
-            return {"type": "relative", "offset": datetime.timedelta()}
+            # this is "now" at this point
+            return self._start
 
         match = re.match(r"^now([+-]\d+)([hmd])$", point)
         if match:
-            offset = int(match.group(1))
+            offset_amount = int(match.group(1))
 
             if match.group(2) == "m":
-                return {'type': "relative", "offset": datetime.timedelta(minutes=offset)}
+                offset = datetime.timedelta(minutes=offset_amount)
+            elif match.group(2) == "h":
+                offset = datetime.timedelta(hours=offset_amount)
+            elif match.group(2) == "d":
+                offset = datetime.timedelta(days=offset_amount)
+            else:
+                raise TimeParsingError("Invalid time format: {}".format(point))
 
-            if match.group(2) == "h":
-                return {'type': "relative", "offset": datetime.timedelta(hours=offset)}
-
-            if match.group(2) == "d":
-                return {'type': "relative", "offset": datetime.timedelta(days=offset)}
+            return self._start + offset
 
         else:
             match = re.match(r"^(\d{4})\D(\d{2})\D(\d{2})\D(\d{2})\D(\d{2})\D(\d{2})$", point)
             if match:
-                dt = datetime.datetime(year=int(match.group(1)),
-                                       month=int(match.group(2)),
-                                       day=int(match.group(3)),
-                                       hour=int(match.group(4)),
-                                       minute=int(match.group(5)),
-                                       second=int(match.group(6)),
-                                       tzinfo=datetime.timezone.utc)
-                return {"type": "absolute", "dt": dt}
-
+                return datetime.datetime(year=int(match.group(1)),
+                                         month=int(match.group(2)),
+                                         day=int(match.group(3)),
+                                         hour=int(match.group(4)),
+                                         minute=int(match.group(5)),
+                                         second=int(match.group(6)),
+                                         tzinfo=datetime.timezone.utc)
             else:
                 match = re.match(r"^(\d{4})\D(\d{2})\D(\d{2})$", point)
                 if match:
-                    dt = datetime.datetime(year=int(match.group(1)),
-                                           month=int(match.group(2)),
-                                           day=int(match.group(3)),
-                                           tzinfo=datetime.timezone.utc)
-                    return {"type": "absolute", "dt": dt}
+                    return datetime.datetime(year=int(match.group(1)),
+                                             month=int(match.group(2)),
+                                             day=int(match.group(3)),
+                                             tzinfo=datetime.timezone.utc)
 
         raise TimeParsingError("Invalid time format: {}".format(point))

commit 104cecc7335aac3d0270083fae103ff2960d87b4

Correct bulk size in bulk param source
With this commit we correct the bulk size to *not* take the
action-and-metadata line into account for the bulk size calculation.
This problem has been introduced in
863d532898db476ba67e336039b4d53b5899e4d8.

eventdata/parameter_sources/elasticlogs_bulk_source.py
@@ -81,7 +81,13 @@ class ElasticlogsBulkSource:
         self.orig_args = [track, params, kwargs]
         self._indices = track.indices
         self._params = params
-        self._randomevent = RandomEvent(params)
+        # we could also do `kwargs.get("random_event, RandomEvent(params))` but that would call the constructor eagerly
+        # which we want to avoid because this can cause significant overhead.
+        if "random_event" in kwargs:
+            self._randomevent = kwargs["random_event"]
+        else:
+            self._randomevent = RandomEvent(params)
+
 
         self._bulk_size = 1000
         if 'bulk-size' in params.keys():
@@ -192,7 +198,8 @@ class ElasticlogsBulkSource:
         response = {
             "body": "\n".join(bulk_array),
             "action-metadata-present": True,
-            "bulk-size": len(bulk_array)
+            # the bulk array contains the action-and-metadata line and the actual document
+            "bulk-size": len(bulk_array) // 2
         }
 
         if "pipeline" in self._params.keys():

tests/parameter_sources/elasticlogs_bulk_source_test.py
@@ -1,0 +1,51 @@
+from eventdata.parameter_sources.elasticlogs_bulk_source import ElasticlogsBulkSource
+
+
+class StaticEventGenerator:
+    def __init__(self, index, type, doc, at_most=-1):
+        self.index = index
+        self.type = type
+        self.doc = doc
+        self.at_most = at_most
+
+    def generate_event(self):
+        if self.at_most == 0:
+            raise StopIteration()
+        self.at_most -= 1
+        return self.doc, self.index, self.type
+
+
+class StaticTrack:
+    def __init__(self):
+        self.indices = []
+
+
+def test_generates_a_complete_bulk():
+    expected_bulk_size = 10
+
+    generator = StaticEventGenerator(index="elasticlogs", type="_doc", doc='{"location": [-0.1485188,51.5250666]}')
+    param_source = ElasticlogsBulkSource(track=StaticTrack(), params={
+        "index": "elasticlogs",
+        "bulk-size": expected_bulk_size
+    }, random_event=generator)
+    client_param_source = param_source.partition(partition_index=0, total_partitions=1)
+
+    generated_params = client_param_source.params()
+    assert len(generated_params["body"].split("\n")) == 2 * expected_bulk_size
+    assert generated_params["action-metadata-present"] is True
+    assert generated_params["bulk-size"] == expected_bulk_size
+
+
+def test_generates_a_bulk_that_ends_prematurely():
+    generator = StaticEventGenerator(index="elasticlogs", type="_doc", doc='{"loc": [-0.14851,51.5250]}', at_most=5)
+    param_source = ElasticlogsBulkSource(track=StaticTrack(), params={
+        "index": "elasticlogs",
+        "bulk-size": 10
+    }, random_event=generator)
+    client_param_source = param_source.partition(partition_index=0, total_partitions=1)
+
+    generated_params = client_param_source.params()
+    # the actual bulk size does not matter but instead that the generator stopped prematurely after 5 items
+    assert len(generated_params["body"].split("\n")) == 10
+    assert generated_params["action-metadata-present"] is True
+    assert generated_params["bulk-size"] == 5