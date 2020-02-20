ce7305b0b30e53bcc2bec6a4637dfdecf7a5dbed
Daniel Mitterdorfer <daniel@mitterdorfer.name>
2020-02-20 15:48:43 +0100

    Use async runners if needed
    Due to elastic/rally#852 we will implement a compatibility layer in the
    current load generator that will also use the asyncio API and thus
    requires custom runners to be registered differently (by specifying
    `async_runner=True`). Rally's runner registry will also expose a new
    attribute `async_runner` that is set to `True` if Rally requires runners
    to be registered as described above.
    
    With this commit we introduce a (temporary) compatibility layer for all
    custom runners that allows older Rally versions to work with the classic
    runners and newer Rally versions with the async runners.
    
    Relates elastic/rally#852

--- eventdata/track.py
+++ eventdata/track.py
@@ -18,22 +18,32 @@
 
 from eventdata.parameter_sources.elasticlogs_bulk_source import ElasticlogsBulkSource
 from eventdata.parameter_sources.elasticlogs_kibana_source import ElasticlogsKibanaSource
-from eventdata.runners import deleteindex_runner
-from eventdata.runners import fieldstats_runner
-from eventdata.runners import indicesstats_runner
-from eventdata.runners import kibana_runner
-from eventdata.runners import nodestorage_runner
-from eventdata.runners import rollover_runner
+from eventdata.runners import deleteindex_runner, deleteindex_runner_async
+from eventdata.runners import fieldstats_runner, fieldstats_runner_async
+from eventdata.runners import indicesstats_runner, indicesstats_runner_async
+from eventdata.runners import kibana_runner, kibana_runner_async
+from eventdata.runners import nodestorage_runner, nodestorage_runner_async
+from eventdata.runners import rollover_runner, rollover_runner_async
 from eventdata.schedulers import utilization_scheduler
 
 
 def register(registry):
+    async_runner = registry.meta_data.get("async_runner", False)
+    if async_runner:
+        registry.register_runner("delete_indices", deleteindex_runner.deleteindex_async, async_runner=True)
+        registry.register_runner("fieldstats", fieldstats_runner.fieldstats_async, async_runner=True)
+        registry.register_runner("indicesstats", indicesstats_runner.indicesstats_async, async_runner=True)
+        registry.register_runner("kibana", kibana_runner.kibana_async, async_runner=True)
+        registry.register_runner("node_storage", nodestorage_runner.nodestorage_async, async_runner=True)
+        registry.register_runner("rollover", rollover_runner.rollover_async, async_runner=True)
+    else:
+        registry.register_runner("delete_indices", deleteindex_runner.deleteindex)
+        registry.register_runner("fieldstats", fieldstats_runner.fieldstats)
+        registry.register_runner("indicesstats", indicesstats_runner.indicesstats)
+        registry.register_runner("kibana", kibana_runner.kibana)
+        registry.register_runner("node_storage", nodestorage_runner.nodestorage)
+        registry.register_runner("rollover", rollover_runner.rollover)
+
     registry.register_param_source("elasticlogs_bulk", ElasticlogsBulkSource)
     registry.register_param_source("elasticlogs_kibana", ElasticlogsKibanaSource)
-    registry.register_runner("delete_indices", deleteindex_runner.deleteindex)
-    registry.register_runner("fieldstats", fieldstats_runner.fieldstats)
-    registry.register_runner("indicesstats", indicesstats_runner.indicesstats)
-    registry.register_runner("kibana", kibana_runner.kibana)
-    registry.register_runner("node_storage", nodestorage_runner.nodestorage)
-    registry.register_runner("rollover", rollover_runner.rollover)
     registry.register_scheduler("utilization", utilization_scheduler.UtilizationBasedScheduler)
