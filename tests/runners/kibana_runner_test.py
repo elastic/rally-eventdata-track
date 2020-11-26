883febd743ac0dfb6f0296b95d3769d0861b72cc
Daniel Mitterdorfer <danielmitterdorfer@users.noreply.github.com>
2020-11-26 11:25:08 +0100

    Handle response errors in Kibana queries (#96)
    With this commit we improve the error handling for Kibana queries.
    Instead of failing with an exception, we handle errors gracefully, set
    the appropriate status and provide more context in meta-data.

--- tests/runners/kibana_runner_test.py
+++ tests/runners/kibana_runner_test.py
@@ -63,6 +63,8 @@ async def test_msearch_without_hits(es):
 
     assert response == {
         "debug": True,
+        "success": True,
+        "error-count": 0,
         "hits": 0,
         "took": 0,
         "weight": 1,
@@ -143,6 +145,8 @@ async def test_msearch_with_hits_as_number(es):
 
     assert response == {
         "debug": True,
+        "success": True,
+        "error-count": 0,
         "hits": 3,
         "took": 7,
         "weight": 1,
@@ -230,9 +234,79 @@ async def test_msearch_with_hits_as_dict(es):
 
     assert response == {
         "debug": True,
+        "success": True,
+        "error-count": 0,
         "hits": 3,
         "took": 9,
         "weight": 1,
         "unit": "ops",
         "visualisation_count": 2,
     }
+
+
+@mock.patch("elasticsearch.Elasticsearch")
+@run_async
+async def test_msearch_with_error(es):
+    params = {
+        "body": [
+            {"index": "elasticlogs-*"},
+            {"query": {"match_all": {}}, "from": 0, "size": 10},
+        ],
+        "meta_data": {
+            "debug": True
+        }
+    }
+    es.msearch.return_value = as_future({
+        "took": 80000,
+        "responses": [
+            {
+                "error": {
+                    "root_cause": [
+                        {
+                            "type": "i_o_exception",
+                            "reason": "failed to read data from cache"
+                        }
+                    ],
+                    "type": "search_phase_execution_exception",
+                    "reason": "all shards failed",
+                    "phase": "fetch",
+                    "grouped": True,
+                    "failed_shards": [
+                        {
+                            "shard": 0,
+                            "index": "logs-2020-01-01",
+                            "node": "Xl9IRaJ6RI-YQRZuBNsbTg",
+                            "reason": {
+                                "type": "i_o_exception",
+                                "reason": "failed to read data from cache"
+                            }
+                        }
+                    ],
+                    "caused_by": {
+                        "type": "i_o_exception",
+                        "reason": "failed to read data from cache",
+                    }
+                },
+                "status": 503
+            }
+        ]
+    })
+
+    response = await kibana_async(es, params=params)
+
+    assert response == {
+        "debug": True,
+        "hits": 0,
+        "took": 80000,
+        "weight": 1,
+        "unit": "ops",
+        "visualisation_count": 1,
+        "success": False,
+        "error-count": 1,
+        "error-type": "kibana",
+        "error-description": "HTTP status: 503, message: all shards failed"
+    }
+
+
+
+
