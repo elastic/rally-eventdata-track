89f5c22611f59528c477884d880caafdab87f32f
Yannick Welsch <yannick@welsch.lu>
2021-04-14 12:31:01 +0200

    Allow rename when mounting searchable snapshot

--- tests/runners/mount_searchable_snapshot_runner_test.py
+++ tests/runners/mount_searchable_snapshot_runner_test.py
@@ -170,3 +170,54 @@ async def test_mount_snapshot_frozen(es):
                   body={"index": "elasticlogs-2018-05-05"},
                   params={"storage": "shared_cache"}),
     ])
+
+@mock.patch("elasticsearch.Elasticsearch")
+@run_async
+async def test_mount_snapshot_with_renames(es):
+    es.snapshot.get.return_value = as_future({
+        "responses": [
+            {
+                "repository": "eventdata",
+                "snapshots": [
+                    {
+                        "snapshot": "eventdata-snapshot",
+                        "uuid": "mWJnRABaSh-gdHF3-pexbw",
+                        "indices": [
+                            "elasticlogs-2018-05-03",
+                            "elasticlogs-2018-05-04",
+                            "elasticlogs-2018-06-05"
+                        ]
+                    }
+                ]
+            }
+        ]
+    })
+    # one call for each matching index
+    es.transport.perform_request.side_effect = [
+        as_future(),
+        as_future(),
+    ]
+
+    params = {
+        "repository": "eventdata",
+        "snapshot": "eventdata-snapshot",
+        "index_pattern": "elasticlogs-2018-05-*",
+        "rename_pattern": "elasticlogs-(.*)",
+        "rename_replacement": "renamed-logs-\\1"
+    }
+
+    runner = MountSearchableSnapshotRunner()
+
+    await runner(es, params=params)
+
+    es.snapshot.get.assert_called_once_with("eventdata", "eventdata-snapshot")
+    es.transport.perform_request.assert_has_calls([
+        mock.call(method="POST",
+                  url="/_snapshot/eventdata/eventdata-snapshot/_mount",
+                  body={"index": "elasticlogs-2018-05-03", "renamed_index": "renamed-logs-2018-05-03"},
+                  params=None),
+        mock.call(method="POST",
+                  url="/_snapshot/eventdata/eventdata-snapshot/_mount",
+                  body={"index": "elasticlogs-2018-05-04", "renamed_index": "renamed-logs-2018-05-04"},
+                  params=None)
+    ])
